20e0c9725b00a7c1d6b845910217d098d1ba60b6
killerwhiteshark <killerwhiteshark@users.noreply.github.com>
2020-01-20 14:31:37 -0800

    Make style adjustments and postioning

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 const methodOverride = require('method-override')
 const app = express();
 const passport = require('passport');
-var session = require('express-session');
+const session = require('express-session');
 const dotenv = require('dotenv').config();
 
 require('./config/database');
@@ -28,9 +28,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({
-  secret: 'mustang345!',
-  resave: false,
-  saveUninitialized: true,
+    secret: 'mustang345!',
+    resave: false,
+    saveUninitialized: true,
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -46,18 +46,18 @@ app.use('/ride', rideRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
