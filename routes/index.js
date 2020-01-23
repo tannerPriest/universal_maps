e16105917c2a2c11aa4b442122cbc0058b955a63
killerwhiteshark <killerwhiteshark@users.noreply.github.com>
2020-01-23 15:11:04 -0800

    Update upperlot review

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 const passport = require('passport');
 const indexRouter = require('../controllers/index')
 
